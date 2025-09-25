b79c73af1fe08a085cdf6d75f5ac533844047572
Nicholas Zhang <nzhan01@bu.edu>
2025-09-25 13:35:10 -0400

    completed all code for mini_insta assignment 3, need comments

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,5 +1,8 @@
 
+from django.urls import path
 
+from .views import ProfileListView, ProfileDetailView
 urlpatterns =[
-
+    path('', ProfileListView.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name='show_profile'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Article
+from .models import Profile
 import random
 # Create your views here.
+
+class ProfileListView(ListView):
+    '''define a view to show all profiles'''
+
+    model= Profile
+    template_name = 'mini_insta/show_all_profiles.html'
+    context_object_name = 'profiles' # should be plural
+
+class ProfileDetailView(DetailView):
+    '''display a single profile'''
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile' # should be singular
