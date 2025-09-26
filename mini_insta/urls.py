6e7af39f87be72c3196457d6660540f129d31092
Nicholas Zhang <nzhan01@bu.edu>
2025-09-26 18:05:42 -0400

    added new styling and comments. ready to deploy

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,4 +1,10 @@
 
+# file: mini_insta/urls.py
+# Nicholas Zhang
+# nzhan01@bu.edu
+# created: 9/25/2025
+# models file containing all the urls for the mini_insta app
+
 from django.urls import path
 
 from .views import ProfileListView, ProfileDetailView
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,8 +1,14 @@
+# file: mini_insta/views.py
+# Nicholas Zhang
+# nzhan01@bu.edu
+# created: 9/25/2025
+# models file containing all the views for the mini_insta app
+
 
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Profile
-import random
+
 # Create your views here.
 
 class ProfileListView(ListView):
