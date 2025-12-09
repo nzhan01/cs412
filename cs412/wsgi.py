b72f03cbfaea540419d823bc2d0621a7c0f2d8c7
Nicholas Zhang <nzhan01@bu.edu>
2025-12-09 03:36:43 -0500

    updated wsgi to activate the venv with django-allauth downloaded on the csa2 server

--- cs412/wsgi.py
+++ cs412/wsgi.py
@@ -8,6 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the project directory to the PYTHONPATH
+sys.path.insert(0, '/home/ugrad/nzhan01/webapps/django/cs412')
+
+# Activate your virtual environment to allow django-allauth to work
+activate_env = '/home/ugrad/nzhan01/webapps/django/cs412/venv/bin/activate_this.py'
+with open(activate_env) as f:
+    exec(f.read(), dict(__file__=activate_env))
+
 
 from django.core.wsgi import get_wsgi_application
 
